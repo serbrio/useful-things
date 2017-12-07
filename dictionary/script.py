7c5195951437f8e69f495fbbc477a362a89d47c4
sergey <s.britvin@corp.mail.ru>
2017-12-07 20:34:10 +0300

    s

--- dictionary/script.py
+++ dictionary/script.py
@@ -10,18 +10,60 @@
 """
 
 from Tkinter import *
+import tkFileDialog
+
+
+def Quit(ev):
+    global root
+    root.destroy()
+
+
+def LoadFile(ev):
+    fn = tkFileDialog.Open(root, filetypes=[('*.txt files', '.txt')]).show()
+    if fn == '':
+        return
+    textbox.delete('1.0', 'end')
+    textbox.insert('1.0', open(fn, 'rt').read())
+
+
+def SaveFile(ev):
+    fn = tkFileDialog.SaveAs(root, filetypes=[('*.txt files', '.txt')]).show()
+    if fn == '':
+        return
+    if not fn.endswith(".txt"):
+        fn += ".txt"
+    open(fn, 'wt').write(textbox.get('1.0', 'end'))
+
 
 root = Tk()
 
-def Hello(event):
-    print "Yet another hello world"
+panelFrame = Frame(root, height=60, bg='gray')
+textFrame = Frame(root, height=340, width=600)
 
-btn = Button(root,                  #родительское окно
-             text="Click me",       #надпись на кнопке
-             width=30,height=5,     #ширина и высота
-             bg="white",fg="black") #цвет фона и надписи
-btn.bind("<Button-1>", Hello)       #при нажатии ЛКМ на кнопку вызывается функция Hello
-btn.pack()                          #расположить кнопку на главном окне
+panelFrame.pack(side='top', fill='x')
+textFrame.pack(side='bottom', fill='both', expand=1)
+
+textbox = Text(textFrame, font='Arial 14', wrap='word')
+scrollbar = Scrollbar(textFrame)
+
+scrollbar['command'] = textbox.yview
+textbox['yscrollcommand'] = scrollbar.set
+
+textbox.pack(side='left', fill='both', expand=1)
+scrollbar.pack(side='right', fill='y')
+
+loadBtn = Button(panelFrame, text='Load')
+saveBtn = Button(panelFrame, text='Save')
+quitBtn = Button(panelFrame, text='Quit')
+
+loadBtn.bind("<Button-1>", LoadFile)
+saveBtn.bind("<Button-1>", SaveFile)
+quitBtn.bind("<Button-1>", Quit)
+
+loadBtn.place(x=10, y=10, width=40, height=40)
+saveBtn.place(x=60, y=10, width=40, height=40)
+quitBtn.place(x=110, y=10, width=40, height=40)
+
 root.mainloop()
 
 
